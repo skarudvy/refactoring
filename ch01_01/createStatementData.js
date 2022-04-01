c17fbf9b2686cefc152d20473ffe8a7401ddca7c
skarudvy <skarudvy0202@gmail.com>
2022-04-01 15:14:02 +0900

    function inline
    fix delete param
    
    inline variable
    
    remove format variable
    
    change function name
    
    destructuring loop
    
    extract function
    
    renderPlaneText
    
    extract
    
    fix amount
    
    fix volumeCredits
    
    fix amount
    
    loop pipeline
    
    interim check
    
    add htmlStatement
    
    fix class
    
    fix class
    
    chpater 01 complete

---
+++ ch01_01/createStatementData.js
@@ -0,0 +1,86 @@
+const createPerformanceCalculator = (aPerformance, aPlay) => {
+    switch(aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default: throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
+    }    
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {        
+        let result = 40000;
+
+        if(this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        
+        if(this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        
+        result += 300 * this.performance.audience;
+
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
+}
+
+const createStatementData = (invoice, plays) => {
+    const enrichPerformance = (aPerformance) => {
+        const calcurator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calcurator.play;
+        result.amount = calcurator.amount;
+        result.volumeCredits = calcurator.volumeCredits;
+        return result;
+    }
+
+    const playFor = (aPerformance) => {
+        return plays[aPerformance.playID];
+    }
+
+    const totalAmount = (data) => {
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }    
+
+    const totalVolumeCredits = (data) => {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    }
+
+    const result = {};
+
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    
+    return result;
+}
+
+export default createStatementData;
--- ch01_01/statement.js
+++ ch01_01/statement.js
@@ -1,56 +1,46 @@
-function amountFor(perf, play) {
-    let thisAmount = 0;
+import createStatementData from "./createStatementData.js";
 
-    switch(play.type) {
-        case "tragedy": 
-            thisAmount = 40000;
-            if(perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if(perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
-            }
-            thisAmount += 300 * perf.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    return thisAmount;
+const statement = (invoice, plays) => {
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
-
-function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
-
-    for(let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
-    
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        
-        //희극 관객 5명마다 추가 포인트를 제공한다.
-        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
-        result += ` ${play.name}: ${format(thisAmount / 100 )} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+const renderPlainText = (data) => {
+    const usd = (aNumber) => {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber/100);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
     
+    for(let perf of data.performances) {
+        // 청구 내역을 출력한다.
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    
+    return result;
+}
+
+const htmlStatement = (invoice, plays) => {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+const renderHtml = (data) => {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for(let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>` 
+        result += `<td>${usd(perf.amount)}</td></tr>\n`
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em> 점</p>\n`;
     return result;
 }
 
